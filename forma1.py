bb8a54c8c640c267d7ed7603df4da2cb0625e796
Gyuris Csaba <gycsaba72@gmail.com>
2022-04-25 10:19:25 +0200

    Update forma1.py

--- forma1.py
+++ forma1.py
@@ -2,40 +2,17 @@ class Eredmeny:
     nev=str()
     maxsebesseg=int()
 
-    def __init__(self,nev,maxsebesseg):
-        self.nev=nev
-        self.maxsebesseg=maxsebesseg
 
 class Verseny:
-    eredmenyek=list()
-
-    def __init__(self,adatok:str):
-        adat=adatok.split(";")
-        for versenyzo in adat:
-            adat=versenyzo.split(",")
-            ve=Eredmeny(adat[0], int(adat[1]))
-            self.eredmenyek.append(ve)
+    eredmenyek=
 
     def  atlag(self)->float:
-        osszeg=0
-        for eredmeny in self.eredmenyek:
-            osszeg=osszeg+eredmeny.maxsebesseg
-        atlag= float(osszeg) / len(self.eredmenyek)
-        return atlag
-
+        return 0
     def leggyorsabb(self)->int:
-        max=self.eredmenyek[0].maxsebesseg
-        for index in range(1,len(self.eredmenyek)):
-            if (self.eredmenyek[index].maxsebesseg>max):
-                max = self.eredmenyek[index].maxsebesseg
-        return max
+        return 0
 
     def leglassabb(self)->int:
-        min=self.eredmenyek[0].maxsebesseg
-        for index in range(1,len(self.eredmenyek)):
-            if (self.eredmenyek[index].maxsebesseg<min):
-                min = self.eredmenyek[index].maxsebesseg
-        return min
+        return 0
 
 
 
